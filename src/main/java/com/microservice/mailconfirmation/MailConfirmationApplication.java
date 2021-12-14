841b08bea9ca6d45f20322b6961015bd6e675934
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-14 19:45:58 -0300

    Initial commit

---
+++ src/main/java/com/microservice/mailconfirmation/MailConfirmationApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.mailconfirmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailConfirmationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailConfirmationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/mailconfirmation/MailConfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.mailconfirmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailConfirmationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
